8297b70ae7117f1961f64ce159bc61c54422b0e9
Kye <ksolomon@csumb.edu>
2017-09-24 17:16:43 +0000

    Finishing Ace Poker game

--- labs/lab3/lab3.php
+++ labs/lab3/lab3.php
@@ -1,58 +1,78 @@
-<?php
-    $deck = range(1,52); // assigns numerical values for each card in the array $deck
+<html>
+    <head>
+        <title>Lab3: Ace Poker</title>
+        <meta charset="utf-8" />
+        <style>
+            .ace{
+                border:yellow 2px solid;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Ace Poker</h1>
+        <h2>Player with more aces wins all points!</h2>
+        <?php
+    $deck = range(1,41); // assigns numerical values for each card in the array $deck
     shuffle($deck);
-    print_r($deck); // prints the cards
+    $totalPoints = 0;
     
 function displayHand(){
-    global $deck;
+    global $deck, $totalPoints;
+    $handPoints=0;
+    $handAces=0;
     for($i=0; $i<5; $i++){
         $lastCard = array_pop($deck);
         $cardValue = $lastCard % 13;
         $cardSuit;
-        if($cardValue<=13){
+        if($lastCard<=13){
             $cardSuit = "clubs";
-        } else if($cardValue>13 && $cardValue<=26){
+        } else if($lastCard>13 && $lastCard<=26){
             $cardSuit = "diamonds";
-        } else if($cardValue>26 && $cardValue<=39){
-            $cardSuit = "heart";
+        } else if($lastCard>26 && $lastCard<=39){
+            $cardSuit = "hearts";
         } else {
             $cardSuit = "spades";
         }
         if($cardValue==0){
             $cardValue=13;
         }
-        echo $lastCard % 13 ." ";
-        echo "<img src='cards/$cardSuit/$cardValue.png' alt='Ace' />";
+        $handPoints = $handPoints + $cardValue;
+        if($cardValue==1){
+            echo "<img class='ace' src='cards/$cardSuit/$cardValue.png' alt='Ace' />";
+            $handAces++;
+        } else {
+            echo "<img src='cards/$cardSuit/$cardValue.png' alt='Ace' />";
+        }
+        
     }
+    echo " Points: " . $handPoints;
+    echo " Aces: " . $handAces;
+    $totalPoints = $totalPoints + $handPoints;
+    return $handAces;
 }
 echo "<hr>";
-displayHand();
+$player1Aces = displayHand();
 echo "<hr>";
-displayHand();
+$player2Aces = displayHand();
 echo "<hr>";
 
-function displayRandomCard(){
-    $suits=array("clubs", "diamonds", "hearts", "spades");
-    echo "<img src='cards/".$suits[rand(0,3)]."/".rand(1,13).".png' alt='Ace' />";
-}
-for($i=0; $i<4; $i++){
-    displayRandomCard();
+function identifyWinner(){
+    global $player1Aces, $player2Aces;
+    if($player1Aces>$player2Aces){
+        echo "Player 1";
+    } else if ($player1Aces<$player2Aces){
+        echo "Player 2";
+    }else{
+        echo "Nobody";
+    }
 }
 ?>
-<html>
-    <head>
-        <title>Lab3: Ace Poker</title>
-        <meta charset="utf-8" />
-        <header>
-            <h1>Ace Poker</h1>
-            <h2>Player with more aces wins all points!</h2>
-        </header>
-        <style>@import url("lab3.css");</style>
-    </head>
-    <body>
-        
+    <h2> <?=identifyWinner()?> wins
+    <?=$totalPoints?>
+    points!</h2>
     </body>
     <footer>
+        <hr width="50%" />
         <h6>&copy;2017 Kye Solomon.</h6>
     </footer>
 </html>
