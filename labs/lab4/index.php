67035fc871c7d103e1c0fc9792afd20e104b4789
Kye <ksolomon@csumb.edu>
2017-10-04 07:55:09 +0000

    Creating pixabay lab

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
     $backgroundImage="slider/img/sea.jpg";
     
@@ -93,3 +94,100 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </body>
 </html>
+=======
+<?php
+    $backgroundImage="slider/img/sea.jpg";
+    
+    // API call goes here
+    if(isset($_GET['keyword'])){
+        include 'slider/api/pixabayAPI.php';
+        $keyword = $_GET['keyword'];
+        $imageURLs = getImageURLs($_GET['keyword']);
+        $backgroundImage = $imageURLs[array_rand($imageURLs)];
+    }
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Image Carousel</title>
+        <meta charset="utf-8">
+        <link href="text/css" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+        <style>
+            @import url("slider/css/styles.css");
+            body{
+                background-image: url('<?=$backgroundImage ?>');
+            }
+        </style>
+    </head>
+    <body>
+        <br /><br />
+        <?php
+            if(!isset($imageURLs)){
+                echo "<h2>Type a keyword to display a slideshow <br /> with random images from Pixabay.com </h2>";
+            } else {
+                // Display carousel here
+        ?>
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+            <!-- Indicators here -->
+            <ol class="carousel-indicators">
+                <?php
+                    for($i=0; $i<5; $i++){
+                        echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
+                        echo ($i == 0)?" class='active'": "";
+                        echo "></li>";
+                    }
+                ?>
+            </ol>
+            <!-- Wrapper for images -->
+            <div class="carousel-inner" role="listbox">
+                <?php
+                    for($i=0; $i<7; $i++){
+                        do{
+                            $randomIndex = rand(0, count($imageURLs));
+                        } while (!isset($imageURLs[$randomIndex]));
+                        echo '<div class= "item ';
+                        echo ($i==0)?"active": "";
+                        echo '">';
+                        echo '<img src"' . $imageURLs[$randomIndex] . '">';
+                        echo '</div>';
+                        unset($imageURLs[$randomIndex]);
+                    
+                    }
+                ?>
+            </div>
+            <!-- Controls here -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+             </a>
+             <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+             </a>
+        </div>
+        <?php
+            }
+        ?>
+        <!-- html form goes here -->
+        <br>
+        <form>
+            <input type="text" name="keyword" placeholder="keyword" value="<?=$_GET['keyword']?>"/>
+            <input type="radio" id="lhorizontal" name="layout" value="horizontal">
+            <label for="Horizontal"></label><label for="lhorizontal"> Horizontal </label>
+            <input type = "radio" id="lvertical" name="layout" value="vertical">
+            <label for="Vertical"></label><label for="lvertical"> Vertical </label>
+            <select name = "category">
+                <option value= "">Select One</option>
+                <option value= "ocean">Sea</option>
+                <option>Forest</option>
+                <option>Mountain</option>
+                <option>Snow</option>
+            </select>
+            <input type="submit" value="Submit" / >
+        </form>
+        <br /><br />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
+>>>>>>> 9f41f267c371ed4ddf99184fdd15fd249850d8e9
