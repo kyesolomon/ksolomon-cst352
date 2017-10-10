54cd815ca91122e7fb4d1b71b2bd9d0b444118af
Kye <ksolomon@csumb.edu>
2017-10-10 10:44:16 +0000

    Creating HW3

---
+++ hw/hw3/hw3.php
@@ -0,0 +1,51 @@
+<?php
+    echo $_POST["name"] ;
+?>$
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ASSIGNMENT 3: HTML Forms / PHP</title>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <style>@import url("hw3.css");</style>
+    </head>
+    <body>
+        <!-- Form goes here -->
+        <form action="confirmation.php"  method="post" >
+            Name:
+            <input type="text" name="name"><br><br>
+            <?php
+                $_POST = name;
+                if ( empty($_POST['name']) ) {
+	            echo "Please enter your name.";
+                } else {
+                echo "<p>Thank you, "  . $_POST['name'];
+                }
+            ?>
+            Undergraduate Level:
+            <select name="standing">
+                <option value="freshman">Freshman</option>
+                <option value="sophomore">Sophomore</option>
+                <option value="junior">Junior</option>
+                <option value="senior">Senior</option>
+            </select><br><br>
+            Gender:<br>
+            <input type="radio" name="gender" value="male" checked>
+            <label for="male">Male</label><br>
+            <input type="radio" name="gender" value="female">
+            <label for="female">Female</label><br>
+            <input type="radio" name="gender" value="other">
+            <label for="other">Other</label>
+            <br><br>
+            Check all that apply to you:<br>
+            <input type="checkbox" name="transportation1" value="Drive"> I drive to school.<br>
+            <input type="checkbox" name="transportation2" value="Bus"> I take the bus to school.<br>
+            <input type="checkbox" name="transportation3" value="Carpool"> I carpool to school.<br><br>
+            <textarea name="message" rows="10" cols="30">Tell us how we can improve your transportation to CSUMB.
+            </textarea><br><br>
+            <button type="button" onclick="alert('You answers are submitted, thanks for taking this survey!')">Submit</button>
+            <input type="reset">
+        </form>
+    </body> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</html>
--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,199 +1,157 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php
-    $backgroundImage="slider/img/sea.jpg";
+include 'slider/api/pixabayAPI.php';
+$backgroundImg = "img/sea.jpg";
+// function getImages() {
+if ( isset($_GET['keyword']) ) {
     
-    // API call goes here
-    if(isset($_GET['keyword'])){
-        include 'slider/api/pixabayAPI.php';
-        $keyword = $_GET['keyword'];
-        $imageURLs = getImageURLs($_GET['keyword']);
-        $backgroundImage = $imageURLs[array_rand($imageURLs)];
+    $keyword = $_GET['keyword']; // default
+    
+    if (!empty($_GET['category']) ) {  // an option was selected in drop down menu
+        $keyword = $_GET['category']; // use category instead
     }
+    
+    if (isset($_GET['layout'])) {
+        $imageURLs = getImageURLs($keyword, $_GET['layout']);
+    } 
+    else {
+         $imageURLs = getImageURLs($keyword);
+    }
+    
+    $randomIndex = array_rand($imageURLs);
+    $backgroundImg = $imageURLs[$randomIndex]; // random background image
+}
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Image Carousel</title>
-        <meta charset="utf-8">
+        <title> </title>
+        <!-- Bootstrap CSS -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- External CSS -->
         <style>
-            @import url("slider/css/styles.css");
-            body{
-                background-image: url('<?=$backgroundImage ?>');
+            @import url('css/styles.css');
+            body {
+                background-image: url('<?=$backgroundImg?>'); /* set background img to whatever value of $backgroundImg is */
+                background-size: cover;
             }
         </style>
     </head>
-    <body>
-        <br /><br />
-        <?php
-            if(!isset($imageURLs)){
-                echo "<h2>Type a keyword to display a slideshow <br /> with random images from Pixabay.com </h2>";
-            } else {
-                // Display carousel here
-        ?>
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Indicators here -->
-            <ol class="carousel-indicators">
-                <?php
-                    for($i=0; $i<5; $i++){
-                        echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
-                        echo ($i == 0)?" class='active'": "";
-                        echo "></li>";
-                    }
-                ?>
-            </ol>
-            <!-- Wrapper for images -->
-            <div class="carousel-inner" role="listbox">
-                <?php
-                    for($i=0; $i<7; $i++){
-                        do{
-                            $randomIndex = rand(0, count($imageURLs));
-                        } while (!isset($imageURLs[$randomIndex]));
-                        echo '<div class= "item ';
-                        echo ($i==0)?"active": "";
-                        echo '">';
-                        echo '<img src"' . $imageURLs[$randomIndex] . '">';
-                        echo '</div>';
-                        unset($imageURLs[$randomIndex]);
-                    
-                    }
-                ?>
-            </div>
-            <!-- Controls here -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-             </a>
-             <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-             </a>
-        </div>
-        <?php
-            }
-        ?>
-        <!-- html form goes here -->
-        <br>
-        <form>
-            <input type="text" name="keyword" placeholder="keyword" value="<?=$_GET['keyword']?>"/>
-            <input type="radio" id="lhorizontal" name="layout" value="horizontal">
-            <label for="Horizontal"></label><label for="lhorizontal"> Horizontal </label>
-            <input type = "radio" id="lvertical" name="layout" value="vertical">
-            <label for="Vertical"></label><label for="lvertical"> Vertical </label>
-            <select name = "category">
-                <option value= "">Select One</option>
-                <option value= "ocean">Sea</option>
-                <option>Forest</option>
-                <option>Mountain</option>
-                <option>Snow</option>
-            </select>
-            <input type="submit" value="Submit" / >
-        </form>
-        <br /><br />
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    </body>
-</html>
-=======
-=======
->>>>>>> 9f41f267c371ed4ddf99184fdd15fd249850d8e9
-<?php
-    $backgroundImage="slider/img/sea.jpg";
     
-    // API call goes here
-    if(isset($_GET['keyword'])){
-        include 'slider/api/pixabayAPI.php';
-        $keyword = $_GET['keyword'];
-        $imageURLs = getImageURLs($_GET['keyword']);
-        $backgroundImage = $imageURLs[array_rand($imageURLs)];
-    }
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Image Carousel</title>
-        <meta charset="utf-8">
-        <link href="text/css" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
-        <style>
-            @import url("slider/css/styles.css");
-            body{
-                background-image: url('<?=$backgroundImage ?>');
-            }
-        </style>
-    </head>
     <body>
-        <br /><br />
-        <?php
-            if(!isset($imageURLs)){
-                echo "<h2>Type a keyword to display a slideshow <br /> with random images from Pixabay.com </h2>";
-            } else {
-                // Display carousel here
-        ?>
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-            <!-- Indicators here -->
-            <ol class="carousel-indicators">
+        
+        <div class="container">
+          
+          <div class="row">
+            <div class="col">
+              <h1>Pixabay Image Search</h1>
+            </div>
+          </div>
+          
+          <div class="row">
+              <!-- form -->
+              <div class="col">
+                  <form>
+                    <input type="text" name="keyword" placeholder="Search images" />
+                    
+                    <div id="layoutStyle">
+                      <input id="lhorizontal" type="radio" name="layout" value="horizontal"/>
+                      <label for="lhorizontal">Horizontal</label>
+                      
+                      <input id="lvertical" type="radio" name="layout" value="vertical" /> 
+                      <label for="lvertical">Vertical</label>              
+                    </div>
+                    
+                    <select name="category">
+                        <option value="">Select One</option>
+                        <option value="ocean">Sea</option>
+                        <option>Forest</option>
+                        <option>Snow</option>
+                    </select>
+                    
+                    <br />
+                    <input type="submit" name="Search"/>
+                    
+                </form>
+        
+                  <?php
+                  
+                  if (!isset($_GET["keyword"])) { // is there any parameter called "keyword" in the URL
+                      echo "<h2>Type a keyword to display a slideshow with random images from Pixabay.com</h2>";
+                  } else { //the form has been submitted
+                      
+                       if ( empty($_GET['keyword']) && empty($_GET['category']) )  {
+                      
+                              echo "<div id='errorMsg'>";
+                              echo "<h2 style='color:red'> ERROR: You must select a category or type a keyword</h2>";
+                              return;
+                              exit; 
+                              echo "</div>";
+                        }
+          
+                      shuffle($imageURLs);
+                      
+                  ?>                
+              </div>
+          </div>
+          
+          <div class="row">
+            <!-- carousel -->
+            <div class="col">
+               <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
                 <?php
-                    for($i=0; $i<5; $i++){
+                    for ($i = 0; $i < 7; $i++) {
                         echo "<li data-target='#carousel-example-generic' data-slide-to='$i'";
                         echo ($i == 0)?" class='active'": "";
                         echo "></li>";
                     }
                 ?>
-            </ol>
-            <!-- Wrapper for images -->
-            <div class="carousel-inner" role="listbox">
-                <?php
-                    for($i=0; $i<7; $i++){
-                        do{
+              </ol>
+            
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner" role="listbox">
+                 <?php
+                    for ($i = 0; $i < 7; $i++) {
+                        do {
                             $randomIndex = rand(0, count($imageURLs));
-                        } while (!isset($imageURLs[$randomIndex]));
-                        echo '<div class= "item ';
-                        echo ($i==0)?"active": "";
+                        }
+                        while (!isset($imageURLs[$randomIndex]));
+                        
+                        echo '<div class="item ';
+                        echo ($i == 0)?"active": "";
                         echo '">';
-                        echo '<img src"' . $imageURLs[$randomIndex] . '">';
+                        echo '<img src="' . $imageURLs[$randomIndex] . '">';
                         echo '</div>';
                         unset($imageURLs[$randomIndex]);
-                    
-                    }
+                    } // end of for loop
                 ?>
-            </div>
-            <!-- Controls here -->
-            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+              </div>
+            
+              <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
                 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
-             </a>
-             <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
                 <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
-             </a>
-        </div>
-        <?php
-            }
-        ?>
-        <!-- html form goes here -->
-        <br>
-        <form>
-            <input type="text" name="keyword" placeholder="keyword" value="<?=$_GET['keyword']?>"/>
-            <input type="radio" id="lhorizontal" name="layout" value="horizontal">
-            <label for="Horizontal"></label><label for="lhorizontal"> Horizontal </label>
-            <input type = "radio" id="lvertical" name="layout" value="vertical">
-            <label for="Vertical"></label><label for="lvertical"> Vertical </label>
-            <select name = "category">
-                <option value= "">Select One</option>
-                <option value= "ocean">Sea</option>
-                <option>Forest</option>
-                <option>Mountain</option>
-                <option>Snow</option>
-            </select>
-            <input type="submit" value="Submit" / >
-        </form>
-        <br /><br />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+              </a>
+            </div>
+            
+            <?php    
+              } // Else
+            ?>
+            
+            </div> <!-- col closing -->
+            
+          </div> <!-- row closing -->
+          
+        </div> <!-- container closing -->
+            
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
     </body>
 </html>
-<<<<<<< HEAD
->>>>>>> 9f41f267c371ed4ddf99184fdd15fd249850d8e9
-=======
->>>>>>> 9f41f267c371ed4ddf99184fdd15fd249850d8e9
