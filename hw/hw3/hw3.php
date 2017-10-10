91f957e60080acb71b671a5da9202ec7b62ce809
Kye <ksolomon@csumb.edu>
2017-10-10 12:37:45 +0000

    Creating forms hw

--- hw/hw3/hw3.php
+++ hw/hw3/hw3.php
@@ -1,6 +1,3 @@
-<?php
-    echo $_POST["name"] ;
-?>$
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,25 +7,17 @@
     </head>
     <body>
         <!-- Form goes here -->
-        <form action="confirmation.php"  method="post" >
-            Name:
-            <input type="text" name="name"><br><br>
-            <?php
-                $_POST = name;
-                if ( empty($_POST['name']) ) {
-	            echo "Please enter your name.";
-                } else {
-                echo "<p>Thank you, "  . $_POST['name'];
-                }
-            ?>
-            Undergraduate Level:
-            <select name="standing">
-                <option value="freshman">Freshman</option>
-                <option value="sophomore">Sophomore</option>
-                <option value="junior">Junior</option>
-                <option value="senior">Senior</option>
-            </select><br><br>
-            Gender:<br>
+        <form>
+        Name:
+        <input type="text" name="name"><br><br>
+        Undergraduate Level:
+        <select name="standing">
+            <option value="freshman">Freshman</option>
+            <option value="sophomore">Sophomore</option>
+            <option value="junior">Junior</option>
+            <option value="senior">Senior</option>
+        </select><br><br>
+        Gender:<br>
             <input type="radio" name="gender" value="male" checked>
             <label for="male">Male</label><br>
             <input type="radio" name="gender" value="female">
@@ -36,16 +25,29 @@
             <input type="radio" name="gender" value="other">
             <label for="other">Other</label>
             <br><br>
-            Check all that apply to you:<br>
+        Check all that apply to you:<br>
             <input type="checkbox" name="transportation1" value="Drive"> I drive to school.<br>
             <input type="checkbox" name="transportation2" value="Bus"> I take the bus to school.<br>
             <input type="checkbox" name="transportation3" value="Carpool"> I carpool to school.<br><br>
             <textarea name="message" rows="10" cols="30">Tell us how we can improve your transportation to CSUMB.
             </textarea><br><br>
-            <button type="button" onclick="alert('You answers are submitted, thanks for taking this survey!')">Submit</button>
+            <input type="button" value="submit"
+            <?php
+            if ( $_GET['name']=="") {
+            ?>
+	            onclick="alert('Please enter your name.')"
+	        <?
+            } else {
+            ?>
+                onclick="alert('Your answers were submitted, thanks for taking this survey!')"
+            <?php
+            }
+            ?>
+            />
             <input type="reset">
         </form>
     </body> 
+        
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </html>
